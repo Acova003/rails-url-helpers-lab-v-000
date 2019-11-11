6989c1e5203c44dce77719e1b234c2d52d70ddb5
Amee Covarrubias <ameecova@yahoo.com>
2019-11-11 22:33:03 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,7 +8,7 @@ class StudentsController < ApplicationController
   def show
   end
   
-  def active 
+  def activate
     @student = Student.find(params[:id])
     if @student.active 
       @student.active = true 
